d4bbd4ea4e4978eb6cdbbf8a6c5cb663cd3c30f2
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-01-29 23:16:27 +0000

    runall: reduce concurrency

--- runall.sh
+++ runall.sh
@@ -9,39 +9,39 @@ n_cli=8
 burst=1024
 for f in 5 4 3 2 1 0; do
     for p in alea iss; do
+        (
         for b in 16 64 128 512; do
-        (
             for l in 128 512 1024 8192 16384; do
                 runone -p $p -f $f -l $l -c $n_cli -b $b -B $burst
             done
-        ) &
         done
+        ) &
     done
 done
 wait
 
 for f in 5 4 3 2 1 0; do
     for p in alea iss; do
+        (
         for b in 1024 2048 4096 7750; do # 8192 is too much
-        (
             for l in 128 512 1024 8192 16384; do
                 runone -p $p -f $f -l $l -c $n_cli -b $b -B $burst
             done
-        ) &
         done
+        ) &
     done
 done
 wait
 
 for f in 5 4 3 2 1 0; do
     for p in alea iss; do
-        for b in 512 1024 2048 4096 7750; do # 8192 is too much
         (
+        for b in 512 1024 2048 4096 7750; do # 8192 is too much
             for l in 32768 65536 131072 262144 524288; do
                 runone -p $p -f $f -l $l -c $n_cli -b $b -B $burst
             done
-        ) &
         done
+        ) &
     done
 done
 wait
