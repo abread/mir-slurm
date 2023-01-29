3d5b2e2bdc0e9470932ff6d247f5ec9fed9af452
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-01-29 18:21:02 +0000

    lower default stat period to 1s

--- lib/runscript.sh
+++ lib/runscript.sh
@@ -66,7 +66,7 @@ RUNONE_OPTS=(
     LOAD/l/load/
     COOLDOWN/C/cooldown/60
     BATCH_SIZE/b/replica-batchSize/
-    STAT_PERIOD/P/replica-statPeriod/5s
+    STAT_PERIOD/P/replica-statPeriod/1s
     BURST/B/client-burst/1024
     DURATION/T/client-duration/120
     REQ_SIZE/s/client-reqSize/256
