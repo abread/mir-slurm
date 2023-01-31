5b9d83f807766419e378f3075454531d6fbbc0fb
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-01-31 17:03:43 +0000

    runmir: properly report status

--- lib/runmir.sh
+++ lib/runmir.sh
@@ -84,7 +84,7 @@ for i in $(seq 0 "$MAX_ATTEMPTS"); do
     RUN_OUT_DIR="$(dirname "$OUTPUT_DIR")/${i}:$(basename "$OUTPUT_DIR")"
     if try_run "$i" && sync && sleep "$RETRY_COOLDOWN" && check_run_ok "$i"; then
         mv "$RUN_OUT_DIR" "$OUTPUT_DIR"
-        break
+        exit 0
     else
         FAIL_OUT_DIR="$(dirname "$OUTPUT_DIR")/FAIL.${i}:$(basename "$OUTPUT_DIR")"
         mv "$RUN_OUT_DIR" "$FAIL_OUT_DIR"
@@ -92,3 +92,5 @@ for i in $(seq 0 "$MAX_ATTEMPTS"); do
 
     echo "run for $OUTPUT_DIR failed, retrying"
 done
+
+exit 1
