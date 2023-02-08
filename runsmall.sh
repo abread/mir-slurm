80a85c32b34945fbceb491e6664f660e45781345
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-02-08 12:21:48 +0000

    add runsmall.sh

---
+++ runsmall.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+# ensure mir bench is up-to-date
+(
+	cd "${CLUSTER_HOME:-..}/mir"
+	srun --cpus-per-task=4 -n1 -- make bin/bench
+)
+sync
+
+source "$(dirname "$0")/lib/runscript.sh"
+
+# also save current mir rev just in case
+(
+	OUTPUT_DIR="$(realpath "$OUTPUT_DIR")"
+	cd "${CLUSTER_HOME:-..}/mir"
+	git rev-parse HEAD > "${OUTPUT_DIR}/mir-version"
+	git diff --staged > "${OUTPUT_DIR}/mir-local-changes.patch"
+)
+
+for p in alea iss; do
+	runone -p $p -f 5 -l 16384 -b 1024
+	runone -p $p -f 1 -l 16 -b 1024
+	runone -p $p -f 5 -l 16384 -b 7680
+	runone -p $p -f 2 -l 8192 -b 1024
+done
