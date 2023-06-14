6d1b9f8b3e16439c5eb98d43046576c00e83677d
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-06-14 15:41:55 +0100

    update processing scripts

--- process-client.py
+++ process-client.py
@@ -9,26 +9,18 @@ import math
 import sys
 
 FIELDS = [
-    'replica_idx',
     'protocol',
+    'client_type',
     'f',
     'intended_load_tps',
     'batch_size',
-    #'n_clients',
     #'burst_size',
-    #'received_txs_count',
-    'observed_load_tps',
+    #'n_clients',
+    'runno',
+    'client_idx',
     #'delivered_txs_count',
     'tps',
     'avg_latency_sec',
-    'mem_sys',
-    'mem_stack_in_use',
-    'mem_heap_alloc',
-    'mem_total_alloc',
-    'mem_malloc_count',
-    'mem_free_count',
-    'mem_pause_total_ns',
-    'mem_pause_count',
     'ok',
 ]
 
@@ -68,7 +60,7 @@ EXP_PARAMETERS_BY_SHORTNAME = {ep.shortname:ep for ep in EXP_PARAMETERS}
 
 ROOT='.'
 def main():
-    all_field_names = set(f.name for f in EXP_PARAMETERS).union(f.name for f in STAT_FIELDS).union(['replica_idx', 'ok'])
+    all_field_names = set(f.name for f in EXP_PARAMETERS).union(f.name for f in STAT_FIELDS).union(['client_idx', 'ok'])
     assert len(all_field_names) == len(EXP_PARAMETERS) + len(STAT_FIELDS) + 2
     assert all(n in all_field_names for n in FIELDS)
 
@@ -88,31 +80,31 @@ def main():
 
         if params["n_clients"] > (params["intended_load_tps"] / 256 + 1):
             params["n_clients"] = params["intended_load_tps"] // 256 + 1
-        n = 3 * params["f"] + 1
+        n = params["n_clients"]
 
-        mintime = min(min(int(rec['ts']) for rec in read_raw_replica_stats(exp_dir, i)) for i in range(0, n))
+        mintime = min(min(float(rec['ts']) for rec in read_raw_client_stats(exp_dir, i)) for i in range(0, n))
 
         exp_cut_end_secs = params['load_duration_sec'] * (100 - EXP_CUT_END_PERCENT) // 100
-        maxtime = mintime + exp_cut_end_secs * 1000
+        maxtime = mintime + exp_cut_end_secs
 
         exp_cut_start_secs = params['load_duration_sec'] * EXP_CUT_START_PERCENT // 100
-        mintime += exp_cut_start_secs * 1000
+        mintime += exp_cut_start_secs
 
-        all_replica_stats = []
-        for replica in range(0,n):
-            raw_replica_stats = read_raw_replica_stats(exp_dir, replica)
-            filtered_replica_stats = filter(time_filter(mintime, maxtime), raw_replica_stats)
+        all_client_stats = []
+        for client in range(0,n):
+            raw_client_stats = read_raw_client_stats(exp_dir, client)
+            filtered_client_stats = filter(time_filter(mintime, maxtime), raw_client_stats)
 
-            replica_stats = parse_replica_stats(map(lambda x: x, filtered_replica_stats))
-            writer.writerow(filter_keys({ 'replica_idx': replica } | params | replica_stats, FIELDS))
+            client_stats = parse_client_stats(map(lambda x: x, filtered_client_stats))
+            writer.writerow(filter_keys({ 'client_idx': client } | params | client_stats, FIELDS))
 
-            all_replica_stats.append(replica_stats)
+            all_client_stats.append(client_stats)
 
-        combined_stats = combine_replica_stats(all_replica_stats)
-        writer.writerow(filter_keys({ 'replica_idx': -1 } | params | combined_stats, FIELDS))
+        combined_stats = combine_client_stats(all_client_stats)
+        writer.writerow(filter_keys({ 'client_idx': -1 } | params | combined_stats, FIELDS))
 
 def time_filter(start, end):
-    return lambda rec: start <= int(rec['ts']) <= end
+    return lambda rec: start <= float(rec['ts']) <= end
 
 @dataclass
 class AverageRes:
@@ -158,44 +150,23 @@ class StatField:
     combiner: any
 
 STAT_FIELDS = [
-    StatField('ts_ms', 'ts', int, None),
-    StatField('received_txs_count', 'nrReceived', int, sum_combiner),
-    StatField('observed_load_tps', 'loadtps', float, avg_combiner),
-    StatField('delivered_txs_count', 'nrDelivered', int, sum_combiner),
+    StatField('ts_s', 'ts', float, None),
+    StatField('delivered_txs_count', 'nrDelivered', int, avg_combiner),
     StatField('tps', 'tps', float, avg_combiner),
     StatField('avg_latency_sec', 'avgLatency', float, avg_combiner),
-    StatField('mem_sys', 'memSys', int, last),
-    StatField('mem_stack_in_use', 'memStackInUse', int, last),
-    StatField('mem_heap_alloc', 'memHeapAlloc', int, last),
-    StatField('mem_total_alloc', 'memTotalAlloc', int, last),
-    StatField('mem_malloc_count', 'memMallocs', int, last),
-    StatField('mem_free_count', 'memFrees', int, last),
-    StatField('mem_pause_total_ns', 'memPauseTotalNs', int, last),
-    StatField('mem_pause_count', 'memNumGC', int, last),
-    StatField('mempool_batch_count', 'mempoolNewBatches', int, sum_combiner),
-    StatField('ag_round_deliver_count', 'agRoundDelivers', int, sum_combiner),
-    StatField('ag_round_false_deliver_count', 'agRoundFalseDelivers', int, sum_combiner),
-    StatField('bc_deliver_count', 'bcDelivers', int, sum_combiner),
-    StatField('tc_queue_size', 'threshQueueSize', int, None),
 ]
 assert len(set(x.name for x in STAT_FIELDS)) == len(STAT_FIELDS)
 assert len(set(x.mir_name for x in STAT_FIELDS)) == len(STAT_FIELDS)
 
 STAT_FIELDS_BY_MIR_NAME = {f.mir_name:f for f in STAT_FIELDS}
 
-def read_raw_replica_stats(exp_dir: str, replica: int):
-    with open(f'{ROOT}/{exp_dir}/replica-{replica}.csv', 'r') as file:
+def read_raw_client_stats(exp_dir: str, client: int):
+    with open(f'{ROOT}/{exp_dir}/client-{client}.csv', 'r') as file:
         reader = csv.DictReader(file)
-        st = 0
         for rec in reader:
-            if st == 0 and rec['nrReceived'] == '1':
-                st = 1 # observed test tx
-            elif st == 1 and rec['nrReceived'] != '0':
-                st = 2 # load started
-            if st == 2:
-                yield rec
+            yield rec
 
-def parse_replica_stats(raw_stats_it):
+def parse_client_stats(raw_stats_it):
     record = {field.name:None for field in filter(lambda f: f.combiner is not None, STAT_FIELDS)}
     record["ok"] = None
 
@@ -225,28 +196,20 @@ def op_and(x, y):
         return y
     return x and y
 
-COMBINED_REPLICA_STATS_FIELDS = {f.name:dataclasses.replace(f) for f in STAT_FIELDS}
-COMBINED_REPLICA_STATS_FIELDS['observed_load_tps'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_sys'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_stack_in_use'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_heap_alloc'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_total_alloc'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_malloc_count'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_free_count'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_pause_total_ns'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['mem_pause_count'].combiner = sum_combiner
-COMBINED_REPLICA_STATS_FIELDS['ok'] = StatField('ok', 'ok', bool, op_and)
-del COMBINED_REPLICA_STATS_FIELDS['ts_ms']
-del COMBINED_REPLICA_STATS_FIELDS['tc_queue_size']
+COMBINED_CLIENT_STATS_FIELDS = {f.name:dataclasses.replace(f) for f in STAT_FIELDS}
+COMBINED_CLIENT_STATS_FIELDS['ok'] = StatField('ok', 'ok', bool, op_and)
+COMBINED_CLIENT_STATS_FIELDS['delivered_txs_count'].combiner = sum_combiner
+COMBINED_CLIENT_STATS_FIELDS['tps'].combiner = sum_combiner
+del COMBINED_CLIENT_STATS_FIELDS['ts_s']
 
-def combine_replica_stats(replica_stats_it):
-    record = {field.name:None for field in COMBINED_REPLICA_STATS_FIELDS.values()}
+def combine_client_stats(client_stats_it):
+    record = {field.name:None for field in COMBINED_CLIENT_STATS_FIELDS.values()}
 
-    for replica_stats in replica_stats_it:
-        assert all(n in replica_stats for n in COMBINED_REPLICA_STATS_FIELDS.keys())
+    for client_stats in client_stats_it:
+        assert all(n in client_stats for n in COMBINED_CLIENT_STATS_FIELDS.keys())
 
-        for k, v in replica_stats.items():
-            field_meta = COMBINED_REPLICA_STATS_FIELDS[k]
+        for k, v in client_stats.items():
+            field_meta = COMBINED_CLIENT_STATS_FIELDS[k]
             record[field_meta.name] = field_meta.combiner(record[field_meta.name], v)
 
     for k, v in record.items():
